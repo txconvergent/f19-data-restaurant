b7d4e2738628fe29508bfe7ca16ced9a3c220ec5
raj8234 <43457820+raj8234@users.noreply.github.com>
2019-11-16 13:29:17 -0600

    new app
    new app

---
+++ my-app/src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+      <div class="navbar">
+          <a class="active">Sort By </a> 
+          <a href="#">Search</a> 
+          <a href="#">Filter</a> 
+
+        </div>
+      
+
+  );
+}
+
+export default Navbar;
---
+++ my-app/src/Restaurant.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Restaurant() {
+  return (
+    <h2>Three Unequal Columns</h2>
+
+  
+  );
+}
+
+export default Restaurant;
